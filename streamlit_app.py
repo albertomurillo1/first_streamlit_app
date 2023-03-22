e4619b915c9c14e27ecc325c9c1d2d0f80412761
albertomurillo1 <128620337+albertomurillo1@users.noreply.github.com>
2023-03-22 16:59:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ import streamlit
 streamlit.title('Ristorante Albertino')
 
 streamlit.header('Menu')
-streamlit.text('Pizza')
+streamlit.text('Pizza 4s')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
