9160f843a1d9e10ab668f194fa1e2b99b85c0a97
albertomurillo1 <128620337+albertomurillo1@users.noreply.github.com>
2023-03-24 09:35:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-import python
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
